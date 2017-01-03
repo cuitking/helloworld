7c95275ba61131091bad527e4ce4c6fa77d07d34
andyvic <andyvic@gmail.com>
2017-01-03 14:13:21 +0800

    update

---
+++ lua_src/src/getms.lua
@@ -0,0 +1,9 @@
+local utime = require "usetime"
+
+local microsecond = utime.getmicrosecond()
+local millisecond = utime.getmillisecond()
+
+print('microsecond',microsecond)
+print('millisecond',millisecond)
+
+----
